b6672f0ecd2fb080aaba6963bd02d734544792b9
luistanachian <53198490+luistanachian@users.noreply.github.com>
2021-02-08 21:09:25 -0300

    create console-mongodb

---
+++ Console-MongoDB/Models/Employees.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Console_MongoDB.Models
+{
+    public class Employees
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        [BsonElement("name")]
+        public string Name { get; set; }
+        [BsonElement("age")]
+        public int Age { get; set; }
+
+    }
+}
---
+++ Console-MongoDB/Program.cs
@@ -0,0 +1,22 @@
+﻿using Console_MongoDB.Models;
+using MongoDB.Driver;
+using System;
+
+namespace Console_MongoDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Console-MongoDB");
+
+            var client = new MongoClient("mongodb://localhost:27017");
+            var testDB = client.GetDatabase("testDB");
+            var employeesDB = testDB.GetCollection<Employees>("Employees");
+
+            var employee = new Employees { Name = "Luis", Age = 31 };
+
+            employeesDB.InsertOne(employee);
+        }
+    }
+}
